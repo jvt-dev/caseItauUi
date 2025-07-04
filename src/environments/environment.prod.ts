9dbc5a35070b87668672c2c89624ebf178c73ade
jvt-dev <joaov_teixeira@hotmail.com>
2025-07-04 12:26:15 -0300

    Altera URL de produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://caseitau-api-1528176482.sa-east-1.elb.amazonaws.com:5000/api'
+  apiUrl: 'http://caseitauapi-1477292961.sa-east-1.elb.amazonaws.com/api'
 };
